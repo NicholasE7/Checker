6404f84a2cb34a71667f0ae8e897b8e8da82c0dd
NicholasE7 <elsnicholas0@gmail.com>
2022-04-28 17:50:46 +0200

    Finish Setting up Realm pods

--- Checker/AppDelegate.swift
+++ Checker/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import CoreData
+import Realm
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        print(Realm.Configuration.defaultConfiguration.encryptionKey)
+        
+
+        do{
+        
+        let realm = try Realm()
+      
+        }catch{
+            
+            print("Error initialising new realm, \(error)")
+            
+        }
         
         return true
     }
---
+++ Checker/Data Model/data.swift
@@ -0,0 +1,18 @@
+//
+//  data.swift
+//  Checker
+//
+//  Created by Nicholas Els on 2022/04/28.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+class Data : Object {
+    
+   @objc dynamic var name : String = ""
+    
+   @objc dynamic var age : Int = 0
+    
+}
